3ce7bf1b4dac465af5aad0bbd9cab86be939b228
吳宇翰 <3b032117@gm.student.ncut.edu.tw>
2023-10-16 16:44:08 +0800

    Route GET r1 設定為重新導向到 r2 (r2 顯示view: welcome)

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,11 @@ Route::get('/', function () {
 Route::get('/', function () {
     return 'welcome';
 });
+
+Route::get('r1', function () {
+    return redirect('r2');
+});
+
+Route::get('r2', function () {
+    return view('welcome');
+});
